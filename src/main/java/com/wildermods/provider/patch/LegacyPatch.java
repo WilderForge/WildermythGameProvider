3bfb175301ca331966d3d6481853461a9bd7ddd1
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-12-27 04:14:38 -0500

    Initial Commit

---
+++ src/main/java/com/wildermods/provider/patch/LegacyPatch.java
@@ -0,0 +1,61 @@
+package com.wildermods.provider.patch;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import net.fabricmc.loader.impl.game.patch.GamePatch;
+import net.fabricmc.loader.impl.launch.FabricLauncher;
+
+public class LegacyPatch extends GamePatch {
+
+	@Override
+	public void process(FabricLauncher launcher, Function<String, ClassReader> classSource,
+			Consumer<ClassNode> classEmitter) {
+		String entrypoint = launcher.getEntrypoint();
+		String gameEntryPoint;
+		ClassNode mainClass = readClass(classSource.apply(entrypoint));
+		
+		if(mainClass == null) {
+			throw new LinkageError ("Could not load main class " + entrypoint + "!");
+		}
+		
+		MethodNode mainMethod = findMethod(mainClass, (method) -> method.name.equals("<clinit>") && method.desc.equals("()V"));
+		
+		if(mainMethod == null) {
+			throw new NoSuchMethodError("Could not find main method in " + entrypoint +  "!");
+		}
+		
+		System.out.println("entrypoint is " + entrypoint);
+		System.out.println("Main method is " + mainMethod.name + mainMethod.desc);
+		
+		gameEntryPoint = entrypoint;
+		
+		ClassNode gameClass;
+		
+		gameClass = readClass(classSource.apply(gameEntryPoint));
+		if(gameClass == null) throw new Error("Could not load game class " + gameEntryPoint + "!");
+		
+		if (gameClass != mainClass) {
+			classEmitter.accept(gameClass);
+		} else {
+			classEmitter.accept(mainClass);
+		}
+		
+	}
+	
+	boolean getName(MethodInsnNode method, String name) {
+		if(method.name.equals(name)) {
+			return true;
+		}
+		else {
+			System.out.println(method.name + method.desc + " is not the main method");
+			return false;
+		}
+	}
+
+}
