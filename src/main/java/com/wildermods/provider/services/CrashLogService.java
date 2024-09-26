2c40ae03fa4dede2f1081c3e593ff8046162e0b6
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2024-09-25 20:13:24 -0400

    Crash Log Service can be overridden

--- src/main/java/com/wildermods/provider/services/CrashLogService.java
+++ src/main/java/com/wildermods/provider/services/CrashLogService.java
@@ -3,15 +3,37 @@ package com.wildermods.provider.services;
 import java.util.ServiceLoader;
 
 public interface CrashLogService {
-
+	
 	public void logCrash(Throwable t);
 	
 	public static CrashLogService obtain(ClassLoader loader) {
-		return ServiceLoader.load(CrashLogService.class, loader).findFirst().orElse(null);
+		return obtain(loader, false);
+	}
+	
+	public static CrashLogService obtain(ClassLoader loader, boolean override) {
+		if(Internal.service == null || override) {
+			Internal.service = ServiceLoader.load(CrashLogService.class, loader).findFirst().orElse(null);
+		}
+		return Internal.service;
 	}
 	
 	public static CrashLogService obtain() {
-		return ServiceLoader.load(CrashLogService.class).findFirst().orElse(null);
+		return obtain(false);
+	}
+	
+	public static CrashLogService obtain(boolean override) {
+		if(Internal.service == null || override) {
+			Internal.service = ServiceLoader.load(CrashLogService.class).findFirst().orElse(null);
+		}
+		return Internal.service;
+	}
+	
+	public static void override(CrashLogService service) {
+		Internal.service = service;
+	}
+	
+	public static class Internal {
+		private static transient CrashLogService service;
 	}
 	
 }
