2962b94dd6c3abeb272fd06fcec9ecf40c3bfeab
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2024-10-28 03:13:17 -0400

    Settings file is now human readable.

--- src/main/java/com/wildermods/provider/ProviderSettings.java
+++ src/main/java/com/wildermods/provider/ProviderSettings.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.SerializedName;
@@ -13,7 +14,7 @@ import com.google.gson.stream.JsonReader;
 
 public class ProviderSettings {
 	
-	private static final Gson GSON = new Gson();
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 	private @SerializedName("_WARNING_WARNING_") String NOTICE = "WARNING - Enabling coremods from the steam workshop does impose a security risk. Additionally, an automatic update to a coremod may break your game unexpectedly. Enabling workshop coremods is not recommended unless you know and understand the risks. Workshop coremod loading may be disabled entirely in future versions if it causes too many issues.";
 	private boolean enableWorkshopCoremods = false;
 	
