bbb8cd9fda54f13d7d2c0e44dbb0a71959ec9a90
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2025-06-05 12:11:23 -0400

    Move logging api to provider

---
+++ src/main/java/com/wildermods/provider/util/logging/LogLevel.java
@@ -0,0 +1,87 @@
+package com.wildermods.provider.util.logging;
+
+import org.apache.logging.log4j.Level;
+
+public enum LogLevel {
+		TRACE(Level.TRACE, net.fabricmc.loader.impl.util.log.LogLevel.TRACE),
+		DEBUG(Level.DEBUG, net.fabricmc.loader.impl.util.log.LogLevel.DEBUG),
+		INFO(Level.INFO, net.fabricmc.loader.impl.util.log.LogLevel.INFO),
+		WARN(Level.WARN, net.fabricmc.loader.impl.util.log.LogLevel.WARN),
+		ERROR(Level.ERROR, net.fabricmc.loader.impl.util.log.LogLevel.ERROR),
+		FATAL(Level.FATAL, net.fabricmc.loader.impl.util.log.LogLevel.ERROR);
+		
+		private final Level log4jLevel;
+		private final net.fabricmc.loader.impl.util.log.LogLevel fabricLevel;
+		
+		LogLevel(Level log4jLevel, net.fabricmc.loader.impl.util.log.LogLevel fabricLevel) {
+			this.log4jLevel = log4jLevel;
+			this.fabricLevel = fabricLevel;
+		}
+		
+		public Level toLog4j() {
+			return log4jLevel;
+		}
+		
+		public net.fabricmc.loader.impl.util.log.LogLevel toFabric() {
+			return fabricLevel;
+		}
+		
+		public static LogLevel getLevel(net.fabricmc.loader.impl.util.log.LogLevel level) {
+			switch(level) {
+				case TRACE:
+					return TRACE;
+				case DEBUG:
+					return DEBUG;
+				default:
+				case INFO:
+					return INFO;
+				case WARN:
+					return WARN;
+				case ERROR:
+					return ERROR;
+			}
+		}
+		
+		public static net.fabricmc.loader.impl.util.log.LogLevel toFabricLevel(LogLevel level) {
+			switch(level) {
+				case TRACE:
+					return net.fabricmc.loader.impl.util.log.LogLevel.TRACE;
+				case DEBUG:
+					return net.fabricmc.loader.impl.util.log.LogLevel.DEBUG;
+				default:
+				case INFO:
+					return net.fabricmc.loader.impl.util.log.LogLevel.INFO;
+				case WARN:
+					return net.fabricmc.loader.impl.util.log.LogLevel.WARN;
+				case ERROR:
+				case FATAL:
+					return net.fabricmc.loader.impl.util.log.LogLevel.ERROR;
+			}
+		}
+		
+		public static LogLevel getLevel(org.apache.logging.log4j.Level level) {
+			LogLevel logLevel = LogLevel.valueOf(level.name());
+			if(logLevel == null) {
+				logLevel = INFO;
+			}
+			return logLevel;
+		}
+		
+		public static LogLevel getLevel(int level) {
+			switch (level) {
+				case 0:
+					return TRACE;
+				case 1:
+					return DEBUG;
+				default:
+				case 2:
+					return INFO;
+				case 3:
+					return WARN;
+				case 4:
+					return ERROR;
+				case 5:
+					return FATAL;
+			}
+		}
+	}
