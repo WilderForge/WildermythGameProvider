57d27a00e2009fb8af951088f01d6737856421d0
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2026-04-29 22:50:34 -0400

    rm unused code

--- src/main/java/com/wildermods/provider/internal/classload/ProviderClassLoader.java
+++ src/main/java/com/wildermods/provider/internal/classload/ProviderClassLoader.java
@@ -2,7 +2,6 @@ package com.wildermods.provider.internal.classload;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
